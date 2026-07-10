6256288c0d0f3a8cc0ebc1554efd0ca2625f4d07
Dharshini <baladharsh533@gmail.com>
2026-07-11 00:45:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import PyPDF2
 
 app = Flask(__name__)
 
-# MySQL Connection
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="authenticity_validator"
+    host=os.environ.get("DB_HOST"),
+    port=int(os.environ.get("DB_PORT")),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_NAME")
 )
 
 cursor = conn.cursor()
